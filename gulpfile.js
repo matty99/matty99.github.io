2d3263a84da5c67ecd4a23a464cc72933154546e
Mateusz Bocian <matbocian91@gmail.com>
2015-12-24 11:29:55 -0500

    Adding gulp task to optimize images

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var pngcrush = require('imagemin-pngcrush');
 
 gulp.task('sass', function () {
   gulp.src('./styles/sass/*.scss')
@@ -11,4 +12,10 @@ gulp.task('sass', function () {
 
 gulp.task('sass:watch', function () {
   gulp.watch('./styles/sass/*.scss', ['sass']);
+});
+
+gulp.task('images', function () {
+  return gulp.src('images/**/*.png')
+    .pipe(pngcrush({reduce: true})())
+	.pipe(gulp.dest('images'));
 });
