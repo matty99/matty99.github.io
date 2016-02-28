ee074186bf70dbb15ca547bf99a2342a79374a10
Mateusz Bocian <matbocian91@gmail.com>
2016-02-28 16:25:31 -0500

    Removing unused gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,8 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
-
-gulp.task('sass', function () {
-  gulp.src('./styles/sass/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./styles/css'));
-});
-
-gulp.task('sass:watch', function () {
-  gulp.watch('./styles/sass/*.scss', ['sass']);
-});
 
 gulp.task('images', function () {
   return gulp.src('images/**/*.png')
