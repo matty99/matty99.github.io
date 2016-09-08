8f461dae1ba1cc34dfb23abba773dbaffa7ffdea
Mateusz Bocian <matbocian91@gmail.com>
2016-09-07 22:10:11 -0400

    Following style recommendations for js

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,17 @@ var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 
 gulp.task('images', function () {
-  return gulp.src('images/**/*.png')
+  return gulp.src(['**/*.png', '**/*.jpg', '**/*.gif', '**/*.svg'])
     .pipe(imagemin({
       optimizationLevel: 7,
       multipass: true,
       progressive: true,
-      svgoPlugins: [{
-        removeViewBox: false
-      }],
-      use: [pngquant()]
+      svgoPlugins: [
+        {
+          removeViewBox: false,
+        },
+      ],
+      use: [pngquant()],
     }))
-    .pipe(gulp.dest('images'));
+    .pipe(gulp.dest('.'));
 });
